1bb19fb727e7ffcbf5117c859983932d10161f33
Gabriel S. Rossi <gabrieldesouzarossi123456@gmail.com>
2023-06-29 19:05:27 -0300

    29/06/2023

---
+++ Xib-UIKit/Xib-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xib-UIKit
+//
+//  Created by Gabriel Rossi on 29/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
